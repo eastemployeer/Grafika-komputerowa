a752c08f4a9a281fc059b9152f1e9c37e848ab98
eastemployeer <50885549+eastemployeer@users.noreply.github.com>
2020-11-24 01:35:56 +0100

    Added egg with light

---
+++ grafika_jajko_swiatlo_zad4/cmake-build-debug/CMakeFiles/grafika_jajko_swiatlo_zad4.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/grafika_jajko_swiatlo_zad4.dir/main.cpp.o"
+  "grafika_jajko_swiatlo_zad4"
+  "grafika_jajko_swiatlo_zad4.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/grafika_jajko_swiatlo_zad4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
